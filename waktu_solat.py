02601a05f26c47dfec94ec626dbef4e42d387235
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-07-08 06:45:34 +0800

    Update

--- waktu_solat.py
+++ waktu_solat.py
@@ -1,91 +1,60 @@
-import requests
 import sys
+import requests
 
-waktu_solat_api = "https://waktu-solat-api.herokuapp.com/api/v1/prayer_times.json"
-
-response = requests.get(waktu_solat_api)
-data = response.json()
-
-# for maklumat in data['data']['negeri'][0]['zon'][0]['waktu_solat']:
-#     print(maklumat['time'])
-
-def grab_api():
+def get_api():
     waktu_solat_api = "https://waktu-solat-api.herokuapp.com/api/v1/prayer_times.json"
     response = requests.get(waktu_solat_api)
     data = response.json()
     return data
 
-# def filter_results(data, negeri, daerah, waktu):
-#     for info in data['data']['negeri']['zon']['waktu_solat']:
-#         nama = info['name']
-#         waktu = info['time']
-#         if info['name'] == waktu_solat_api:
-#             print(f'Waktu {nama} adalah pada: {waktu}')
-
-def filter_results(data, waktu):
-    for info in data['data']['negeri']['zon']['waktu_solat']:
-        waktu = info['time']
-        if waktu == "None":
-            print(info)
-        # elif daerah == NULL:
-        #     print(data['data']['negeri'])
-        # elif negeri == NULL:
-        #     print(data['data'])
-        # else: print(data['data'])
-
-# def pilihan(pilih, daerahVar):
-#     if pilih == negeri:
-#         print("Sila masukkan nama daerah")
-#         daerahVar = input()
-        
-#         print(data['data']['negeri'])
-
-
 def get_input():
-    negeri = sys.argv[1]
-    daerah = sys.argv[2]
-    waktu  = sys.argv[3]
-    # print(f'Negeri: {negeri} Daerah: {daerah} Waktu: {waktu}')
-    return str(negeri), str(daerah), str(waktu)
+    if len(sys.argv) == 4:
+        input_negeri = sys.argv[1]
+        input_zon    = sys.argv[2]
+        input_waktu  = sys.argv[3]
+    elif len(sys.argv) == 3:
+        input_negeri = sys.argv[1]
+        input_zon    = sys.argv[2]
+        input_waktu  = ''
+    elif len(sys.argv) == 2:
+        input_negeri = sys.argv[1]
+        input_zon    = ''
+        input_waktu  = ''
+    elif len(sys.argv) == 1:
+        input_negeri = ''
+        input_zon    = ''
+        input_waktu  = ''
+    return input_negeri, input_zon, input_waktu
+
+def data_filter(input_negeri, input_zon, input_waktu, data):
+    for arkib in data['data']['negeri']:
+        nama1 = arkib['nama']
+        if input_negeri == nama1:
+            for info in arkib['zon']:
+                nama2 = info['nama']
+                if input_zon == nama2:
+                    for maklumat in info['waktu_solat']:
+                        nama3 = maklumat['name']
+                        if input_waktu == nama3:
+                            print(maklumat)
+                            break
+                    else:
+                        print(info['waktu_solat'])
+                    break
+            else:
+                print(arkib['zon'])
+            break
+    else:
+        print(data['data']['negeri'])
 
 def main():
-    # try:
-        negeri, daerah, waktu = get_input()
-        data = grab_api()
-        filter_results()
 
-    # except Exception as e:
-    #     print("Tak jadi bro")
+    try:
+        data = get_api()
+        input_negeri, input_zon, input_waktu = get_input()
+        data_filter(input_negeri, input_zon, input_waktu, data)
+
+    except Exception as e:
+        print("Input salah")
 
 main()
-
-'''
-###############
-# PSEUDO CODE #
-###############
-
-- grab_api()
-    - ambik API
-    - ambik "data" from .JSON
-    - release "data"
-
-- get_input()
-    - input_1 = Negeri
-    - input_2 = Daerah
-    - input_3 = Waktu
-    - release "Negeri", "Daerah", "Waktu"
-
-- filter_result()
-    - ambik data dari "data"
-    - "Negeri", "Daerah", "Waktu" 
-            - python waktu_solat.py Negeri Daerah Waktu
-            - Print Waktu spesifik
-    - "Negeri", "Daerah"          
-            - python waktu_solat.py Negeri Daerah
-            - Print Waktu seluruh "Daerah"
-    - "Negeri"                    
-            - python waktu_solat.py Negeri
-            - "Sila tulis "Daerah"
-            - input "Daerah"
-            - Print Waktu seluruh "Daerah"
-'''
